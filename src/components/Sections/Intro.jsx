2146c74f268df0bc9994ae4399fa0f98798a943c
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-01-16 12:52:16 +0530

    Fix: Changed Image

--- src/components/Sections/Intro.jsx
+++ src/components/Sections/Intro.jsx
@@ -43,7 +43,7 @@ function Intro() {
 
                 </Col>
                 <Col className={`${classes.introImage}`}>
-                    <img className='mt-lg-0 mt-5' src='https://res.cloudinary.com/deuyeqft4/image/upload/v1673635479/Public/profile-pic_r3nqpf.png' alt='Author'/>
+                    <img className='mt-lg-0 mt-5' src='https://res.cloudinary.com/deuyeqft4/image/upload/v1673853480/Public/ray_akbjns.png' alt='Author'/>
                 </Col>
             </Row>
         </Container>
